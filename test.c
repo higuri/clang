05b4be2dacbb1983aa42931de092ef55dac3aad8
higuri <yhigu@yhigu-no-Mac-mini.local>
2018-01-27 18:36:08 +0900

    Add wmain()

--- test.c
+++ test.c
@@ -1,1 +1,13 @@
-// main.c// main()int main() {    return 0;}
+// test.c
+
+#include <assert.h>
+
+static void run_tests() {
+    assert(0);
+}
+
+// main()
+int main() {
+    run_tests();
+    return 0;
+}
